5f7e69629362fdde610d9ce3a7d78535cc617e29
Amudhavalli R <amudhameenaravi@gmail.com>
2024-02-26 10:49:31 +0530

    changed validation

--- src/EmployeeValidation.js
+++ src/EmployeeValidation.js
@@ -12,6 +12,7 @@ function Validation({
   const id_pattern = /^\d+$/
   const salary_pattern = /^\d+$/
   const dob_pattern = /^\d{4}-\d{2}-\d{2}$/
+  const name_pattern = /^[a-zA-Z\s]+$/
 
   if (id === '') {
     error.id = 'ID should not be empty'
@@ -23,6 +24,8 @@ function Validation({
 
   if (name === '') {
     error.name = 'Name should not be empty'
+  } else if (!name_pattern.test(name)) {
+    error.name = 'Invalid name format'
   } else if (name.length > 30) {
     error.name = 'Name should not exceed 30 characters'
   } else {
@@ -45,10 +48,7 @@ function Validation({
     error.salary = 'Salary should not be empty'
   } else if (!salary_pattern.test(salary)) {
     error.salary = 'Invalid salary format'
-  } else if (
-    parseInt(salary) < 1 ||
-    parseInt(salary) > 99999999
-  ) {
+  } else if (parseInt(salary) < 1 || parseInt(salary) > 99999999) {
     error.salary = 'Salary must be in the range 1 to 99999999'
   } else {
     error.salary = ''
@@ -58,15 +58,19 @@ function Validation({
     error.dob = 'DOB should not be empty'
   } else if (!dob_pattern.test(dob)) {
     error.dob = 'Invalid DOB format'
-  }else {
-  const selectedDate = new Date(dob);
-  const currentDate = new Date();
-  if (selectedDate > currentDate) {
-    error.dob = 'DOB should not be a future date';
   } else {
-    error.dob = '';
+    const selectedDate = new Date(dob)
+    const currentDate = new Date()
+    selectedDate.setHours(0, 0, 0, 0) // Set hours, minutes, seconds, and milliseconds to 0 for comparison
+    currentDate.setHours(0, 0, 0, 0)
+    if (selectedDate > currentDate) {
+      error.dob = 'DOB should not be a future date'
+    } else if (selectedDate.getTime() === currentDate.getTime()) {
+      error.dob = 'DOB should not be present date'
+    } else {
+      error.dob = ''
+    }
   }
-}
 
   if (gender === '') {
     error.gender = 'Gender should not be empty'
